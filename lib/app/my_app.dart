623b008e3411c35e5e04fb02ab816a651001ec0f
Akhmadjon Akbarov <akhmadjonakbarov@gmail.com>
2025-12-03 20:42:31 +0500

    ref: Page was changed

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:learning_clean_code/features/home/view/home_page.dart';
+import 'package:learning_clean_code/features/product/presentation/product_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Material App', home: HomePage());
+    return MaterialApp(title: 'Material App', home: ProductListPage());
   }
 }
